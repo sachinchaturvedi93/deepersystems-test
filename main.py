b509335786b86ef7cb705f26083917f935bc52ef
Sachin Chaturvedi <sachin93@gmail.com>
2020-08-16 02:36:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open('source_file_2.json') as json_data:
     data = json.load(json_data)
 #pprint(data)
 
-# Sorting it by Ascending order based onpriority
+# Sorting it by ascending order based on priority, lower priority number means higher priority.
 lists =  (sorted(data, key = lambda i:i['priority']))
 pprint(lists)
 manager = []
@@ -14,14 +14,16 @@ for items in lists:
     manager.append(items['managers'])
     watcher.append(items['watchers'])
     
-
+# Taking out the list of managers and watchers
 import itertools
 merged_manager = list(itertools.chain.from_iterable(manager))
 merged_watcher = list(itertools.chain.from_iterable(watcher))
 
+# converting them to a dictionary removes all duplicates, converting them back to lists
 merged_manager = list(dict.fromkeys(merged_manager))
 merged_watcher = list(dict.fromkeys(merged_watcher))
 
+# Appending names to dictionary and adding
 manager_json = dict()
 for i in merged_manager:    
     for items in lists:
@@ -36,7 +38,7 @@ for i in merged_watcher:
             watcher_json.setdefault(i, []).append(items['name'])
 print(watcher_json)
 
-
+# Writing into JSON files as requested
 json_object_1 = json.dumps(manager_json, indent=4)
 json_object_2 = json.dumps(watcher_json, indent=4)
 with open("managers.json", "w") as outfile:
